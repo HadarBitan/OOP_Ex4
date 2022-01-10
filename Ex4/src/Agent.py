35d2ff043b00ed56038b63a6a9df2d53461c9d09
Hadar Bitan <86705118+HadarBitan@users.noreply.github.com>
2022-01-10 09:11:07 +0200

    Add files via upload

---
+++ Ex4/src/Agent.py
@@ -0,0 +1,108 @@
+from src.Graph.Point3D import Point3D
+from src.Pokemon import Pokemon
+# import threading
+
+
+class Agent:
+    """
+    This class represent a single agent shown by:
+    id -> an integer that can not be changed
+    value -> a float number representing the score of the agent and that grows every time
+        the agent catches a pokemon
+    src -> a source vertex where the agent comes from
+    dest -> the agents destination
+    speed -> an float number represent the speed of the agent and grows every time
+        the agent catches a pokemon
+    pos -> a 3 dimensional point represent the current position of the agent
+    """
+
+    def __init__(self, agent_id: int, value: float, src: int, dest: int, speed: float, pos: tuple, client):
+        # threading.Thread.__init__(self)
+        self._id: int = agent_id
+        self._value: float = value
+        self._src: int = src
+        self._dest: int = dest
+        self._speed: float = speed
+        self._pos: tuple = (float(pos[0]), float(pos[1]), float(pos[2]))
+        self._pokemon = None
+        # self.sum_moves = 0
+        # self._client = client
+
+    def get_id(self) -> int:
+        return self._id
+
+    def get_value(self) -> float:
+        return self._value
+
+    def get_src(self) -> int:
+        return self._src
+
+    def get_dest(self) -> int:
+        return self._dest
+
+    def get_speed(self) -> float:
+        return self._speed
+
+    def get_pos(self) -> tuple:
+        return self._pos
+
+    def get_pokemon(self):
+        return self._pokemon
+
+    def set_value(self, new_value: float) -> None:
+        """
+        This function update the value of the agent after each time the agent catches a pokemon
+        param: new_value is the value to update
+        """
+        self._value += new_value
+
+    def set_src(self, new_src: int) -> None:
+        """
+        This function update the src node of the agent aka the new node the agent going out
+        from to catch the pokemon
+        param: new_src is the src to update
+        """
+        self._src = new_src
+
+    def set_dest(self, new_dest: int) -> None:
+        """
+        This function update the dest node of the agent aka the new destination the agent going to
+        in purpose to catch the pokemon
+        param: new_dest is the destination to update
+        """
+        self._dest = new_dest
+
+    def set_speed(self, new_speed: float) -> None:
+        """
+        This function update the speed of the agent after each time the agent catches a pokemon
+        param: new_speed is the speed to update
+        """
+        self._speed += new_speed
+
+    def set_pos(self, new_pos: tuple) -> None:
+        """
+        This function update the current position of the agent
+        param: new_pos is the new current position of the agent
+        """
+        self._pos = new_pos
+
+    def set_pokemon(self, new_pokemon: Pokemon):
+        self._pokemon = new_pokemon
+
+    # def run(self) -> None:
+    #     EPS = 0.0000001
+    #     while True:
+    #         # sent the client to the dest of the edge of the pokemon
+    #         self._client.choose_next_edge(
+    #             '{"agent_id":' + str(self._id) + ', "next_node_id":' + str(self._pokemon.get_edge()[1]) + '}')
+    #         # only when the agent is very close to the pokemon act
+    #         point_pok = Point3D(self._pokemon.get_pos()[0], self._pokemon.get_pos()[1], self._pokemon.get_pos()[2])
+    #         point_self = Point3D(self._pos[0], self._pos[1], self._pos[2])
+    #         if point_self.distance(point_pok) <= EPS:
+    #             # final move to catch the pokemon
+    #             self._client.move()
+    #             self.sum_moves += 1
+    #             self._dest = -1
+    #             # setting the pokemon as cached
+    #             break
+
---
+++ Ex4/src/Graph/GraphAlgo.py
@@ -0,0 +1,117 @@
+import json
+import math
+from types import SimpleNamespace
+from typing import List
+
+from src.Graph.DiGraph import DiGraph
+from queue import PriorityQueue
+
+
+class GraphAlgo:
+
+    def __init__(self, graph: DiGraph = None):
+        self.__graph = DiGraph()
+        if graph is not None:
+            for node in graph.get_all_v().values():
+                self.__graph.add_node(node.get_ID(), (node.get_location().get_x(), node.get_location().get_y(),
+                                                      node.get_location().get_z()))
+            for src in graph.get_all_src_dict().keys():
+                for dest in graph.all_out_edges_of_node(src).keys():
+                    self.__graph.add_edge(src, dest, graph.get_edge_weigth(src, dest))
+
+    def get_graph(self) -> DiGraph:
+        return self.__graph
+
+    def load_from_json(self, graph_json: str):
+        graph_out = json.loads(graph_json, object_hook=lambda json_dict: SimpleNamespace(**json_dict))
+        graph = DiGraph()
+        for n in graph_out.Nodes:
+            x, y, _ = n.pos.split(',')
+            n.pos = SimpleNamespace(x=float(x), y=float(y))
+            id_ = n.id
+            graph.add_node(id_, (float(x), float(y), 0.0))
+        for e in graph_out.Edges:
+            src = e.src
+            dest = e.dest
+            w = e.w
+            graph.add_edge(src, dest, float(w))
+        self.__graph = graph
+
+    def save_to_json(self, file_name: str) -> bool:
+        dictionary = {"Edges": [], "Nodes": []}
+        for src in self.__graph.get_all_src_dict().keys():
+            for dest in self.__graph.get_all_src_dict()[src].keys():
+                ed_dict = {"src": src, "w": self.__graph.get_edge_weigth(src, dest), "dest": dest}
+                dictionary["Edges"].append(ed_dict)
+        for node in self.__graph.get_all_v().values():
+            pos = "{},{},{}".format(node.get_location().get_x(), node.get_location().get_y(),
+                                    node.get_location().get_z())
+            id_node = node.get_ID()
+            no_dict = {"pos": pos, "id": id_node}
+            dictionary["Nodes"].append(no_dict)
+
+        try:
+            json_object = json.dumps(dictionary, indent=4)
+            with open(file_name, 'w') as outfile:  # Open a file for writing
+                outfile.write(json_object)
+                return True
+        except Exception as e:
+            print(e)
+            return False
+
+    def _Dijkstra(self, src) -> (list[int], list[int]):
+        dist = {src: 0}
+        for node in self.__graph.get_all_v().keys():
+            if node == src:
+                continue
+            dist[node] = math.inf
+        parantMap = {}
+        for node in self.__graph.get_all_v().keys():
+            parantMap[node] = None
+        heap_queue = PriorityQueue()
+        heap_queue.put((0, src))
+        while not heap_queue.empty():
+            v = heap_queue.get()
+            if v[1] not in self.__graph.get_all_src_dict():
+                break
+            for neighbor in self.__graph.all_out_edges_of_node(v[1]).keys():
+                old_dist = dist[neighbor]
+                new_dist = dist[v[1]] + self.__graph.get_edge_weigth(v[1], neighbor)
+                if new_dist < old_dist:
+                    dist[neighbor] = new_dist
+                    parantMap[neighbor] = v[1]
+                    heap_queue.put((new_dist, neighbor))
+        return parantMap, dist
+
+    def shortest_path(self, id1: int, id2: int) -> (float, list):
+        if id1 not in self.__graph.get_all_v() or id2 not in self.__graph.get_all_v():
+            return math.inf, []
+        if id1 is id2:
+            return 0, [id1]
+        parent, shortest_path_dist = self._Dijkstra(id1)
+        if shortest_path_dist[id2] == math.inf:
+            return math.inf, []
+        path = []
+        if id2 in parent:
+            v = id2
+            while v is not None:
+                path.append(v)
+                v = parent[v]
+        return shortest_path_dist[id2], path[::-1]
+
+    def TSP(self, node_lst: List[int]) -> (List[int], float):
+        sum_weight = 0
+        path = []
+        if len(node_lst) > 0:
+            path.append(node_lst[0])
+            src = node_lst[0]
+            for node in node_lst[1:]:
+                calculate = self.shortest_path(src, node)   #return the short path from the src in all the Graph
+                way = calculate[1]  #[list]
+                sum_weight += calculate[0] #[weight]
+                src = node
+                for p in way[1:]:
+                    path.append(p)
+        if sum_weight == math.inf:
+            return [], math.inf
+        return path, sum_weight
